abe08454a0e27cbcf5ea152cc4b56f923ebee999
Sebastian Dziubak <dziubaczek@gmail.com>
2025-12-15 11:00:49 +0100

    wykonano zadanie 4_1

---
+++ lesson_4/4_1.py
@@ -0,0 +1,30 @@
+"""
+Zadanie 1 – Kalkulator BMI
+Napisz program, który obliczy wskaźnik BMI (Body Mass Index).
+Dane:
+Waga: 70 kg
+Wzrost: 1.75 m
+Wymagania:
+Stwórz zmienne waga i wzrost
+Oblicz BMI według wzoru: BMI = waga / (wzrost²)
+Wyświetl wynik z 2 miejscami po przecinku
+Dodaj interpretację: BMI < 18.5 (niedowaga), 18.5-24.9 (norma), ≥25 (nadwaga)
+
+"""
+
+waga = 70
+wzrost = 1.75
+
+bmi = waga / (wzrost **2)
+interpretacja =''
+
+if 18.5 <= bmi <= 24.9:
+    interpretacja = 'norma'
+elif bmi < 18.5:
+    interpretacja = 'niedowaga'
+else:
+    interpretacja = 'nadwaga'
+
+print(f'BMI: {bmi:.2f}')
+print(f'Interpretacja: {interpretacja.title()}')
+
